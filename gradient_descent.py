540859480105e9aca76690ae7e04ea1129f812fd
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-19 13:27:27 -0500

    compute cost function created

--- gradient_descent.py
+++ gradient_descent.py
@@ -5,3 +5,15 @@ import matplotlib.pyplot as plt
 x_train = np.array([1.0, 2.0])   #features
 y_train = np.array([300.0, 500.0])   #target value
 
+#Function to calculate the cost
+def compute_cost(x, y, w, b):
+   
+    m = x.shape[0] 
+    cost = 0
+    
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = cost + (f_wb - y[i])**2
+    total_cost = 1 / (2 * m) * cost
+
+    return total_cost
