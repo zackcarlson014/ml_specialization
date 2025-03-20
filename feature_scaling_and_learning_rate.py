22e076907aa116f7b9abc046cc1314544863fd35
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-20 17:39:00 -0500

    scatter plot training data

--- feature_scaling_and_learning_rate.py
+++ feature_scaling_and_learning_rate.py
@@ -7,3 +7,10 @@ np.set_printoptions(precision=2)
 
 X_train, y_train = load_house_data()
 X_features = ['size(sqft)','bedrooms','floors','age']
+
+fig,ax=plt.subplots(1, 4, figsize=(12, 3), sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i],y_train)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("Price (1000's)")
+plt.show()
