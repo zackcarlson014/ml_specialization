db5b20a98c697ded9b03eb78e56b0539bf9f1087
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-20 16:47:47 -0500

    additional imports

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,6 +1,7 @@
 import math, copy
 import numpy as np
 import matplotlib.pyplot as plt
+from lab_utils_uni import plt_house_x, plt_contour_wgrad, plt_divergence, plt_gradients
 
 x_train = np.array([1.0, 2.0])   #features
 y_train = np.array([300.0, 500.0])   #target value
