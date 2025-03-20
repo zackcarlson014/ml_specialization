852696e8ac688e71a2460a3a2c380f7af3dab20e
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-20 17:22:25 -0500

    feature scaling and learning rate file created, imports

---
+++ feature_scaling_and_learning_rate.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_multi import  load_house_data, run_gradient_descent 
+from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
+from lab_utils_common import dlc
+np.set_printoptions(precision=2)
+
+X_train, y_train = load_house_data()
+X_features = ['size(sqft)','bedrooms','floors','age']
