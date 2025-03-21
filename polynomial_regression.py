d2b3aeae1904a0ac93a70f29399dd5cf746f2ac5
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-20 19:03:38 -0500

    polynomial regression created

---
+++ polynomial_regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_multi import zscore_normalize_features, run_gradient_descent_feng
+np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
+
+# create target data
+x = np.arange(0, 20, 1)
+y = 1 + x**2
+X = x.reshape(-1, 1)
+
+model_w,model_b = run_gradient_descent_feng(X,y,iterations=1000, alpha = 1e-2)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("no feature engineering")
+plt.plot(x,X@model_w + model_b, label="Predicted Value");  plt.xlabel("X"); plt.ylabel("y"); plt.legend(); plt.show()
