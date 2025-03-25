519bdc61439c9250b32389c221718e3ab7b4d7b1
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-24 19:42:44 -0500

    logistic regression file created, imports

---
+++ logistic_regression.py
@@ -0,0 +1,7 @@
+import numpy as np
+%matplotlib widget
+import matplotlib.pyplot as plt
+from plt_one_addpt_onclick import plt_one_addpt_onclick
+from lab_utils_common import draw_vthresh
+plt.style.use('./deeplearning.mplstyle')
+
