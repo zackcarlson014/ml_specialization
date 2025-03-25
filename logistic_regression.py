07240ee7d11e2e098f1d2af3c5cf94df6fa385d5
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-24 19:44:43 -0500

    numpy exp examples

--- logistic_regression.py
+++ logistic_regression.py
@@ -5,3 +5,16 @@ from plt_one_addpt_onclick import plt_one_addpt_onclick
 from lab_utils_common import draw_vthresh
 plt.style.use('./deeplearning.mplstyle')
 
+# Input is an array. 
+input_array = np.array([1,2,3])
+exp_array = np.exp(input_array)
+
+print("Input to exp:", input_array)
+print("Output of exp:", exp_array)
+
+# Input is a single number
+input_val = 1  
+exp_val = np.exp(input_val)
+
+print("Input to exp:", input_val)
+print("Output of exp:", exp_val)
