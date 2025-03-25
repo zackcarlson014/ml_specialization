23e9704afe70ba57d112e0320f9db8e9f4993024
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-24 19:49:11 -0500

    plot sigmoid

--- logistic_regression.py
+++ logistic_regression.py
@@ -50,3 +50,35 @@ y = sigmoid(z_tmp)
 np.set_printoptions(precision=3) 
 print("Input (z), Output (sigmoid(z))")
 print(np.c_[z_tmp, y])
+
+# Input (z), Output (sigmoid(z))
+# [[-1.000e+01  4.540e-05]
+#  [-9.000e+00  1.234e-04]
+#  [-8.000e+00  3.354e-04]
+#  [-7.000e+00  9.111e-04]
+#  [-6.000e+00  2.473e-03]
+#  [-5.000e+00  6.693e-03]
+#  [-4.000e+00  1.799e-02]
+#  [-3.000e+00  4.743e-02]
+#  [-2.000e+00  1.192e-01]
+#  [-1.000e+00  2.689e-01]
+#  [ 0.000e+00  5.000e-01]
+#  [ 1.000e+00  7.311e-01]
+#  [ 2.000e+00  8.808e-01]
+#  [ 3.000e+00  9.526e-01]
+#  [ 4.000e+00  9.820e-01]
+#  [ 5.000e+00  9.933e-01]
+#  [ 6.000e+00  9.975e-01]
+#  [ 7.000e+00  9.991e-01]
+#  [ 8.000e+00  9.997e-01]
+#  [ 9.000e+00  9.999e-01]
+#  [ 1.000e+01  1.000e+00]]
+
+# Plot z vs sigmoid(z)
+fig,ax = plt.subplots(1,1,figsize=(5,3))
+ax.plot(z_tmp, y, c="b")
+
+ax.set_title("Sigmoid function")
+ax.set_ylabel('sigmoid(z)')
+ax.set_xlabel('z')
+draw_vthresh(ax,0)
