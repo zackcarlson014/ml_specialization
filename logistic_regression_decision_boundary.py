c759e050782bf192e6351f614e936453189ed9c8
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-25 07:43:09 -0500

    initial data

--- logistic_regression_decision_boundary.py
+++ logistic_regression_decision_boundary.py
@@ -2,3 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lab_utils_common import plot_data, sigmoid, draw_vthresh
 plt.style.use('./deeplearning.mplstyle')
+
+X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1) 
