268946a783276f5d05c5705446011e3e654204f1
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-25 07:43:28 -0500

    plot data

--- logistic_regression_decision_boundary.py
+++ logistic_regression_decision_boundary.py
@@ -5,3 +5,11 @@ plt.style.use('./deeplearning.mplstyle')
 
 X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
 y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1) 
+
+fig,ax = plt.subplots(1,1,figsize=(4,4))
+plot_data(X, y, ax)
+
+ax.axis([0, 4, 0, 3.5])
+ax.set_ylabel('$x_1$')
+ax.set_xlabel('$x_0$')
+plt.show()
