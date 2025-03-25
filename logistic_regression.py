e36cf72a816b3927acf9abb4d3a7c02b39c7a143
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-24 19:48:25 -0500

    create sigmoid values array

--- logistic_regression.py
+++ logistic_regression.py
@@ -39,3 +39,14 @@ def sigmoid(z):
     g = 1/(1+np.exp(-z))
    
     return g
+
+# Generate an array of evenly spaced values between -10 and 10
+z_tmp = np.arange(-10,11)
+
+# Use the function implemented above to get the sigmoid values
+y = sigmoid(z_tmp)
+
+# Code for pretty printing the two arrays next to each other
+np.set_printoptions(precision=3) 
+print("Input (z), Output (sigmoid(z))")
+print(np.c_[z_tmp, y])
