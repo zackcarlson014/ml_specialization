ecb9a05055b935a2f614ef6b33084111bfaebb9c
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-25 07:42:25 -0500

    logtistic regression import adjusted

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,7 @@
 import numpy as np
-%matplotlib widget
 import matplotlib.pyplot as plt
 from plt_one_addpt_onclick import plt_one_addpt_onclick
-from lab_utils_common import draw_vthresh
+from lab_utils_common_3 import draw_vthresh
 plt.style.use('./deeplearning.mplstyle')
 
 # Input is an array. 
