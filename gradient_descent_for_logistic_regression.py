369180025979a140a7f744a793f66b8549889abf
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 20:07:24 -0500

    plot data set

--- gradient_descent_for_logistic_regression.py
+++ gradient_descent_for_logistic_regression.py
@@ -7,3 +7,12 @@ plt.style.use('./deeplearning.mplstyle')
 
 X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
 y_train = np.array([0, 0, 0, 1, 1, 1])
+
+# PLOT DATA
+fig,ax = plt.subplots(1,1,figsize=(4,4))
+plot_data(X_train, y_train, ax)
+
+ax.axis([0, 4, 0, 3.5])
+ax.set_ylabel('$x_1$', fontsize=12)
+ax.set_xlabel('$x_0$', fontsize=12)
+plt.show()
