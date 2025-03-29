cb95b751ac5425332f9ef7d86dc7c8771fe1590d
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:14:20 -0500

    plot example logistic loss data

--- logistic_loss.py
+++ logistic_loss.py
@@ -5,3 +5,7 @@ from plt_logistic_loss import soup_bowl, plt_logistic_squared_error
 plt.style.use('./deeplearning.mplstyle')
 
 soup_bowl()
+
+x_train = np.array([0., 1, 2, 3, 4, 5],dtype=np.longdouble)
+y_train = np.array([0,  0, 0, 1, 1, 1],dtype=np.longdouble)
+plt_simple_example(x_train, y_train)
