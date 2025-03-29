90105bef4bda8b9393ed2b5df2d885b2892d0ef5
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:37:17 -0500

    cost for logistic regression function

--- cost_function_for_logistic_regression.py
+++ cost_function_for_logistic_regression.py
@@ -5,3 +5,37 @@ plt.style.use('./deeplearning.mplstyle')
 
 X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])  #(m,n)
 y_train = np.array([0, 0, 0, 1, 1, 1])                                           #(m,)
+
+# PLOT DATA
+fig,ax = plt.subplots(1,1,figsize=(4,4))
+plot_data(X_train, y_train, ax)
+
+# Set both axes to be from 0-4
+ax.axis([0, 4, 0, 3.5])
+ax.set_ylabel('$x_1$', fontsize=12)
+ax.set_xlabel('$x_0$', fontsize=12)
+plt.show()
+
+def compute_cost_logistic(X, y, w, b):
+    """
+    Computes cost
+
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      cost (scalar): cost
+    """
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)
+             
+    cost = cost / m
+    return cost
