d5c58480aba79fa25c09dca445345cfb7ca86040
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:34:30 -0500

    plot squared error, loss curves, and cost function

--- logistic_loss.py
+++ logistic_loss.py
@@ -6,6 +6,19 @@ plt.style.use('./deeplearning.mplstyle')
 
 soup_bowl()
 
+# PLOT LOGISTIC REGRESSION EXAMPLE DATA - CATEGORICAL
 x_train = np.array([0., 1, 2, 3, 4, 5],dtype=np.longdouble)
 y_train = np.array([0,  0, 0, 1, 1, 1],dtype=np.longdouble)
 plt_simple_example(x_train, y_train)
+
+# PLOT LOGISTIC LOSS SQUARED ERROR
+plt.close('all')
+plt_logistic_squared_error(x_train,y_train)
+plt.show()
+
+# PLOT LOGISTIC LOSS CURVES
+plt_two_logistic_loss_curves()
+
+# PLOT LOGISTIC LOSS COST FUNCTION
+plt.close('all')
+cst = plt_logistic_cost(x_train,y_train)
