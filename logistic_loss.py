03755b0a09fd796491daccb86f0114a0756195bd
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:11:54 -0500

    removed comment

--- logistic_loss.py
+++ logistic_loss.py
@@ -1,5 +1,4 @@
 import numpy as np
-%matplotlib widget
 import matplotlib.pyplot as plt
 from plt_logistic_loss import  plt_logistic_cost, plt_two_logistic_loss_curves, plt_simple_example
 from plt_logistic_loss import soup_bowl, plt_logistic_squared_error
