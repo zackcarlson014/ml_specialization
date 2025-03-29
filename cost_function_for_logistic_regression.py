3d5ca3c8222a875f1ebe87626904f4d48a919c64
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:39:21 -0500

    compute cost for different values w,b

--- cost_function_for_logistic_regression.py
+++ cost_function_for_logistic_regression.py
@@ -44,3 +44,36 @@ def compute_cost_logistic(X, y, w, b):
 w_tmp = np.array([1,1])
 b_tmp = -3
 print(compute_cost_logistic(X_train, y_train, w_tmp, b_tmp))
+
+# PLOT DATA WITH TWO EXAMPLE DECISION BOUNDARIES
+# Choose values between 0 and 6
+x0 = np.arange(0,6)
+
+# Plot the two decision boundaries
+x1 = 3 - x0
+x1_other = 4 - x0
+
+fig,ax = plt.subplots(1, 1, figsize=(4,4))
+# Plot the decision boundary
+ax.plot(x0,x1, c=dlc["dlblue"], label="$b$=-3")
+ax.plot(x0,x1_other, c=dlc["dlmagenta"], label="$b$=-4")
+ax.axis([0, 4, 0, 4])
+
+# Plot the original data
+plot_data(X_train,y_train,ax)
+ax.axis([0, 4, 0, 4])
+ax.set_ylabel('$x_1$', fontsize=12)
+ax.set_xlabel('$x_0$', fontsize=12)
+plt.legend(loc="upper right")
+plt.title("Decision Boundary")
+plt.show()
+
+
+# COMPUTE COST FOR DIFFERENT VALUES OF w AND b
+w_array1 = np.array([1,1])
+b_1 = -3
+w_array2 = np.array([1,1])
+b_2 = -4
+
+print("Cost for b = -3 : ", compute_cost_logistic(X_train, y_train, w_array1, b_1))
+print("Cost for b = -4 : ", compute_cost_logistic(X_train, y_train, w_array2, b_2))
