7aa3a68c5b2d87d5863d337624d5d903965607a0
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:36:07 -0500

    initial data

--- cost_function_for_logistic_regression.py
+++ cost_function_for_logistic_regression.py
@@ -2,3 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lab_utils_common_3 import  plot_data, sigmoid, dlc
 plt.style.use('./deeplearning.mplstyle')
+
+X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])  #(m,n)
+y_train = np.array([0, 0, 0, 1, 1, 1])                                           #(m,)
