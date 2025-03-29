4c93f5814f647d785e760010b85ea61b5ad4466f
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 20:07:01 -0500

    data set

--- gradient_descent_for_logistic_regression.py
+++ gradient_descent_for_logistic_regression.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 from lab_utils_common_3 import  dlc, plot_data, plt_tumor_data, sigmoid, compute_cost_logistic
 from plt_quad_logistic import plt_quad_logistic, plt_prob
 plt.style.use('./deeplearning.mplstyle')
+
+X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_train = np.array([0, 0, 0, 1, 1, 1])
