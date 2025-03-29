f4a3dd2d312df4c0a9f0119616f9457ce25eaf36
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:35:40 -0500

    import updated

--- cost_function_for_logistic_regression.py
+++ cost_function_for_logistic_regression.py
@@ -1,4 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from lab_utils_common import  plot_data, sigmoid, dlc
+from lab_utils_common_3 import  plot_data, sigmoid, dlc
 plt.style.use('./deeplearning.mplstyle')
