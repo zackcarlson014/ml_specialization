dd3ddfe4c7067b2051f1253d9f42fc0ac04c53ee
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:11:44 -0500

    logistic loss created, imports

---
+++ logistic_loss.py
@@ -0,0 +1,6 @@
+import numpy as np
+%matplotlib widget
+import matplotlib.pyplot as plt
+from plt_logistic_loss import  plt_logistic_cost, plt_two_logistic_loss_curves, plt_simple_example
+from plt_logistic_loss import soup_bowl, plt_logistic_squared_error
+plt.style.use('./deeplearning.mplstyle')
