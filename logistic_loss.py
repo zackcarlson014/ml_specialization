42f4d2e9213a5593d07c184ccdb881c31615049b
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 19:14:05 -0500

    plot soup_bowl from logistic loss import

--- logistic_loss.py
+++ logistic_loss.py
@@ -3,3 +3,5 @@ import matplotlib.pyplot as plt
 from plt_logistic_loss import  plt_logistic_cost, plt_two_logistic_loss_curves, plt_simple_example
 from plt_logistic_loss import soup_bowl, plt_logistic_squared_error
 plt.style.use('./deeplearning.mplstyle')
+
+soup_bowl()
