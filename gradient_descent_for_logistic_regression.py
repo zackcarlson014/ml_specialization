6e2ce2d6b4c42e456de53a07fff9b735dbce13b1
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-28 20:06:45 -0500

    gradient descent for logistic regression, imports

---
+++ gradient_descent_for_logistic_regression.py
@@ -0,0 +1,6 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_common_3 import  dlc, plot_data, plt_tumor_data, sigmoid, compute_cost_logistic
+from plt_quad_logistic import plt_quad_logistic, plt_prob
+plt.style.use('./deeplearning.mplstyle')
