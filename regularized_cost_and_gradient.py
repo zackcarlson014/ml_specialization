c387f1da9aa22ba238316601a075a6f14395b09c
zackcarlson014 <jzcarlson014@gmail.com>
2025-04-13 20:19:51 -0500

    compute gradient linear regression

--- regularized_cost_and_gradient.py
+++ regularized_cost_and_gradient.py
@@ -114,3 +114,14 @@ def compute_gradient_linear_reg(X, y, w, b, lambda_):
         dj_dw[j] = dj_dw[j] + (lambda_/m) * w[j]
 
     return dj_db, dj_dw
+
+np.random.seed(1)
+X_tmp = np.random.rand(5,3)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1])
+b_tmp = 0.5
+lambda_tmp = 0.7
+dj_db_tmp, dj_dw_tmp =  compute_gradient_linear_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+
+print(f"dj_db: {dj_db_tmp}", )
+print(f"Regularized dj_dw:\n {dj_dw_tmp.tolist()}", )
