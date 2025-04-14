4747a39666d6123b167ee205af045f0cc06c63a6
zackcarlson014 <jzcarlson014@gmail.com>
2025-04-13 20:17:57 -0500

    compute_cost_logistic_reg function created

--- regularized_cost_and_gradient.py
+++ regularized_cost_and_gradient.py
@@ -42,3 +42,34 @@ lambda_tmp = 0.7
 cost_tmp = compute_cost_linear_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
 
 print("Regularized cost:", cost_tmp)
+
+def compute_cost_logistic_reg(X, y, w, b, lambda_ = 1):
+    """
+    Computes the cost over all examples
+    Args:
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns:
+      total_cost (scalar):  cost 
+    """
+
+    m,n  = X.shape
+    cost = 0.
+    for i in range(m):
+        z_i = np.dot(X[i], w) + b                                      #(n,)(n,)=scalar, see np.dot
+        f_wb_i = sigmoid(z_i)                                          #scalar
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)      #scalar
+             
+    cost = cost/m                                                      #scalar
+
+    reg_cost = 0
+    for j in range(n):
+        reg_cost += (w[j]**2)                                          #scalar
+    reg_cost = (lambda_/(2*m)) * reg_cost                              #scalar
+    
+    total_cost = cost + reg_cost                                       #scalar
+    return total_cost 
