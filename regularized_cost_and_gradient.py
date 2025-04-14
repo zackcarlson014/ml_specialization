2daba1d24162e6d11ea6a548265a50e20974f666
zackcarlson014 <jzcarlson014@gmail.com>
2025-04-13 20:19:19 -0500

    corrected import

--- regularized_cost_and_gradient.py
+++ regularized_cost_and_gradient.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from plt_overfit import overfit_example, output
-from lab_utils_common import sigmoid
+from lab_utils_common_3 import sigmoid
 np.set_printoptions(precision=8)
 
 def compute_cost_linear_reg(X, y, w, b, lambda_ = 1):
