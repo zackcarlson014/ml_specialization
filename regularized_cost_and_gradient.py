22d2e486f92e509e3bea4d2051734a1243ca79db
zackcarlson014 <jzcarlson014@gmail.com>
2025-04-13 20:17:13 -0500

    compute regularized cost

--- regularized_cost_and_gradient.py
+++ regularized_cost_and_gradient.py
@@ -31,4 +31,14 @@ def compute_cost_linear_reg(X, y, w, b, lambda_ = 1):
     reg_cost = (lambda_/(2*m)) * reg_cost                              #scalar
     
     total_cost = cost + reg_cost                                       #scalar
-    return total_cost        
+    return total_cost
+
+np.random.seed(1)
+X_tmp = np.random.rand(5,6)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1]).reshape(-1,)-0.5
+b_tmp = 0.5
+lambda_tmp = 0.7
+cost_tmp = compute_cost_linear_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+
+print("Regularized cost:", cost_tmp)
